e864f85617c6b790bfa1ea45865b750898d5c68e
Amanda S. Dufek <amanda.dufek@gmail.com>
2025-03-25 17:28:36 -0700

    fix directory name

--- job_script_dgemm.sh
+++ job_script_dgemm.sh
@@ -3,7 +3,7 @@
 #SBATCH -q debug
 #SBATCH -A nstaff
 #SBATCH -t 00:30:00
-#SBATCH -N 2
+#SBATCH -N 4
 #SBATCH --gpus-per-task=1
 #SBATCH --ntasks-per-node=4
 #SBATCH --job-name=dgemmjob
@@ -22,8 +22,8 @@ if [[ -z "$JOBID" ]]; then
     exit 1
 fi
 
-mkdir -p ${JOBID}
+mkdir -p ${HOME}/${JOBID}
 
-srun --gpus-per-task 1 --ntasks-per-node 4 bash -c 'node_name=$(hostname | tr -c "[:alnum:]" "_"); '$HOME'/python-dgemm.py --nsize 4096 --accelerator >> '${JOBID}'/${node_name}.out 2>&1'
+srun --gpus-per-task 1 --ntasks-per-node 4 bash -c 'node_name=$(hostname | tr -c "[:alnum:]" "_"); '$HOME'/python-dgemm.py --nsize 4096 --accelerator >> '${HOME}'/'${JOBID}'/${node_name}.out 2>&1'
 
 
