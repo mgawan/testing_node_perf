8a344d0bf2385e399de6d0d56e8906034ecd8cc2
Amanda S. Dufek <amanda.dufek@gmail.com>
2025-03-25 18:36:26 -0700

    minor adjustments

--- job_script_dgemm.sh
+++ job_script_dgemm.sh
@@ -4,8 +4,8 @@
 #SBATCH -A nstaff
 #SBATCH -t 00:30:00
 #SBATCH -N 4
-#SBATCH --gpus-per-task=1
-#SBATCH --ntasks-per-node=4
+#SBATCH --gpus-per-task 1
+#SBATCH --ntasks-per-node 4
 #SBATCH --job-name=dgemmjob
 #SBATCH --output=dgemmjob.out
 #SBATCH --error=dgemmjob.err
